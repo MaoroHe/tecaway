a84f267a3a810f664f77654a5eddfaeebd241395
Denis Mathieu <denismathieu1203@gmail.com>
2024-03-17 04:42:42 +0100

    yeah

--- main.js
+++ main.js
@@ -3,7 +3,18 @@ let tram = document.querySelector(".tram");
 
 button.addEventListener('click', (e) => {
 
-    //tram.classList.toggle("glide");
     tram.style.display = "block";
 
-})
+    setTimeout(delText, 2700);
+    
+    function delText(){
+        let home = document.querySelector(".home");
+        home.style.display = "none";
+
+        setTimeout(launchChat, 2000);
+
+        function launchChat(){
+            window.location.replace("chat.html");
+        }
+    }
+});
