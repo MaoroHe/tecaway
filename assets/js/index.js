891fa7f9ec59a69d1aa10cbe23b166f763722e8a
Denis Mathieu <denismathieu1203@gmail.com>
2024-03-21 22:03:21 +0100

    ye

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,3 +11,21 @@ document.querySelector("#inputChat").addEventListener("keyup", (f) => {
         chatting();
     }
 })
+
+document.querySelector("#inputChat").addEventListener("keyup", (f) => {
+    if(f.key === "a"){
+        document.querySelector("#inputChat").value="Quels sont les horaires de la ligne 123 ?";
+    }
+})
+
+document.querySelector("#inputChat").addEventListener("keyup", (f) => {
+    if(f.key === "z"){
+        document.querySelector("#inputChat").value="J'aimerais me rendre à dampremy vers 17h15";
+    }
+})
+
+document.querySelector("#inputChat").addEventListener("keyup", (f) => {
+    if(f.key === "e"){
+        document.querySelector("#inputChat").value="J'ai un souci récurrent, ça fait plusieurs semaines qu'il y a des travaux sur la ligne 456";
+    }
+})
